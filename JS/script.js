45335a3c7691239d4fa486210f81aa0428057a9e
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-11-07 19:20:48 +0000

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,5 @@
 //NewBookBtn
 const form = document.querySelector(".form-container");
-form.style.display === "none"
 form.style.display = "none";
 document.getElementById("btnNewBook").addEventListener("click", () => {
     if(form.style.display === "none") {
@@ -12,6 +11,8 @@ document.getElementById("btnNewBook").addEventListener("click", () => {
 });
 
 //Library
+let myLibrary = [];
+
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
@@ -22,28 +23,31 @@ document.getElementById("submitBtn").addEventListener("click", (e) => {
     let authorValue = author.value;
     let pagesValue = pages.value;
     console.log(title.value, author.value, pages.value);
-    console.log(titleValue === true);
-    if(title.value === true && author.value === true && pages.value === true) {
-        console.log("yes");
+    if(title.value !== "" && author.value !== "" && pages.value !== "") {
         title.value = author.value = pages.value = "";
         form.style.display = "none";
-    }
-
-    
-
-    
+        const newBook = new Book(titleValue, authorValue, pagesValue);
+        myLibrary.push(newBook);
+        console.log(myLibrary);
+        addBookToLibrary();
+    };
 })
 
-let myLibrary = [
-
-];
-console.log(myLibrary);
-
-function Book() {
-
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 function addBookToLibrary() {
+    const flexDiv = document.querySelector(".flex-container");
+    console.log(myLibrary[myLibrary.length - 1]);
+    let currentBook = myLibrary[myLibrary.length - 1];
+    console.log(currentBook.title);
+    const bookCard = document.querySelector(".card");
+    const newCard = bookCard.cloneNode(true);
+    console.log(newCard);
+    flexDiv.appendChild(newCard);
 
 }
 
@@ -52,3 +56,26 @@ function addBookToLibrary() {
 function deleteBook() {
 
 }
+
+
+// function addBookToLibrary() {
+//     console.log(myLibrary[myLibrary.length - 1]);
+//     let currentBook = myLibrary[myLibrary.length - 1];
+//     console.log(currentBook.title);
+//     let flexDiv = document.createElement("div");
+//     flexDiv.className = "flex-container";
+//     let cardDiv = document.createElement("div");
+//     cardDiv.className = "card";
+//     flexDiv.appendChild(cardDiv);
+//     let titleH2 = document.createElement("h2");
+//     titleH2.className = "card-title";
+//     titleH2.textContent = currentBook.title;
+//     cardDiv.appendChild(titleH2);
+//     let authorH5 = document.createElement("h5");
+//     authorH5.className = "card-author";
+//     authorH5.textContent = currentBook.author;
+//     cardDiv.appendChild(authorH5);
+//     let span = document.createElement("span");
+//     span.textContent = currentBook.pages;
+//     cardDiv
+// }
